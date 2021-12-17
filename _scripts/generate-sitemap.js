717cf01a0634746ad7fbe53600f38c41a19737be
leejooy96 <leejooy96@gmail.com>
2021-12-17 09:22:02 +0900

    use environment variables in generate sitemap instead of static domain string

--- _scripts/generate-sitemap.js
+++ _scripts/generate-sitemap.js
@@ -1,12 +1,15 @@
+require('dotenv').config({ path: '.env.production' });
+
 const sitemap = require('nextjs-sitemap-generator');
 const fs = require('fs');
 const path = require('path');
 
 const BUILD_ID = fs.readFileSync('.next/BUILD_ID').toString();
+
 // eslint-disable-next-line no-console
 console.log(`BUILD ID: ${BUILD_ID}`);
 
-const DOMAIN = 'https://example.com';
+const DOMAIN = process.env.NEXT_PUBLIC_BASE_URL;
 
 (async () => {
   await sitemap({
